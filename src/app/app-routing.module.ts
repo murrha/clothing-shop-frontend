04eb54d879c63fd9b66ee99cfa29d33ccda1de4f
Marja <murrha@outlook.com>
2022-11-05 22:43:33 -0700

    fixed aspect ratio of pictures

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
@@ -9,6 +10,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path:'cart', component: CartComponent},
+  {path:'category/:name', component: CategoryListComponent},
   {path: 'product/:name', component: ProductDetailsComponent},
   {path: 'product', component: ProductListComponent},
   {path: '', redirectTo: '/product', pathMatch: 'full'}
